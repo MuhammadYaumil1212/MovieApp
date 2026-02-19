40962f297d28f8301a0afd078498ab7a21c08e22
yaumil <yaumilramkhadani@gmail.com>
2026-02-19 20:08:38 +0700

    add new constant

---
+++ app/src/main/java/yr/muhammadyaumil/movieapp/commons/constants/AppConstants.kt
@@ -0,0 +1,8 @@
+package yr.muhammadyaumil.movieapp.commons.constants
+
+import yr.muhammadyaumil.movieapp.BuildConfig
+
+class AppConstants {
+    val apiKey = BuildConfig.API_KEY
+    val baseUrl = BuildConfig.BASE_URL
+}
