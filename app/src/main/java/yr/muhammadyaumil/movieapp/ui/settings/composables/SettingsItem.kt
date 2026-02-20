2d5e068895e4a0fab519e4f8a06ab5694f7aaa58
yaumil <yaumilramkhadani@gmail.com>
2026-02-20 17:30:56 +0700

    settings akun and films

--- app/src/main/java/yr/muhammadyaumil/movieapp/ui/settings/composables/SettingsItem.kt
+++ app/src/main/java/yr/muhammadyaumil/movieapp/ui/settings/composables/SettingsItem.kt
@@ -6,11 +6,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowForwardIos
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -53,10 +54,14 @@ fun SettingsItem(
                 if (leadingIcon != null) {
                     Icon(leadingIcon, contentDescription = null)
                 }
-                Text(text = title, fontWeight = FontWeight.W500, fontSize = 18.sp)
+                Text(text = title, fontWeight = FontWeight.W400, fontSize = 14.sp)
             }
             if (trailingIcon != null) {
-                Icon(trailingIcon, contentDescription = null)
+                Icon(
+                    imageVector = trailingIcon,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.secondary,
+                )
             }
         }
     }
--- app/src/main/java/yr/muhammadyaumil/movieapp/ui/settings/screens/SettingsScreen.kt
+++ app/src/main/java/yr/muhammadyaumil/movieapp/ui/settings/screens/SettingsScreen.kt
@@ -1,16 +1,27 @@
 package yr.muhammadyaumil.movieapp.ui.settings.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Movie
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.VideoLibrary
+import androidx.compose.material.icons.automirrored.outlined.AirplaneTicket
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Link
+import androidx.compose.material.icons.outlined.Movie
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.VideoLibrary
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.compose.AppTheme
 import yr.muhammadyaumil.movieapp.ui.settings.composables.SettingsItem
 
@@ -22,18 +33,69 @@ data class SettingsItem(
 @Suppress("ktlint:standard:function-naming")
 @Composable
 fun SettingsScreen(modifier: Modifier = Modifier) {
-    val listItem =
+    val listItemAkun =
         listOf(
-            SettingsItem(icon = Icons.Filled.Person, title = "Profile"),
-            SettingsItem(icon = Icons.Filled.Movie, title = "My Films"),
-            SettingsItem(icon = Icons.Filled.VideoLibrary, title = "Watchlist"),
+            SettingsItem(icon = Icons.Outlined.Person, title = "Edit Profile"),
+            SettingsItem(icon = Icons.AutoMirrored.Outlined.AirplaneTicket, title = "My Ticket"),
+            SettingsItem(icon = Icons.Outlined.Link, title = "Akun Terhubung"),
         )
-    LazyColumn(verticalArrangement = Arrangement.Center) {
-        items(listItem) { item ->
-            SettingsItem(
-                leadingIcon = item.icon,
-                title = item.title,
-            )
+
+    val listItemFilm =
+        listOf(
+            SettingsItem(icon = Icons.Outlined.Movie, title = "My Films"),
+            SettingsItem(icon = Icons.Outlined.VideoLibrary, title = "Watchlist"),
+            SettingsItem(icon = Icons.Outlined.Favorite, title = "Favorite"),
+        )
+    Column {
+        Text(
+            text = "Akun",
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(start = 16.dp),
+        )
+        Box(modifier = modifier.padding(16.dp)) {
+            Column(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .border(
+                            width = 1.dp,
+                            color = MaterialTheme.colorScheme.outline,
+                            shape = RoundedCornerShape(8.dp),
+                        ).padding(vertical = 8.dp),
+            ) {
+                listItemAkun.forEach { item ->
+                    SettingsItem(
+                        leadingIcon = item.icon,
+                        title = item.title,
+                    )
+                }
+            }
+        }
+        Text(
+            text = "Film",
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(start = 16.dp),
+        )
+        Box(modifier = modifier.padding(16.dp)) {
+            Column(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .border(
+                            width = 1.dp,
+                            color = MaterialTheme.colorScheme.outline,
+                            shape = RoundedCornerShape(8.dp),
+                        ).padding(vertical = 8.dp),
+            ) {
+                listItemFilm.forEach { item ->
+                    SettingsItem(
+                        leadingIcon = item.icon,
+                        title = item.title,
+                    )
+                }
+            }
         }
     }
 }
