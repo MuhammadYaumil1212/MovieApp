bdd8e13faaae98e8faebad14fad1390bdcb13cab
yaumil <yaumilramkhadani@gmail.com>
2026-02-20 20:54:53 +0700

    slicing profile account

--- app/src/main/java/yr/muhammadyaumil/movieapp/ui/home/screens/HomeScreen.kt
+++ app/src/main/java/yr/muhammadyaumil/movieapp/ui/home/screens/HomeScreen.kt
@@ -1,20 +1,28 @@
 package yr.muhammadyaumil.movieapp.ui.home.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -57,6 +65,26 @@ fun HomeScreen(modifier: Modifier) {
                 LazyColumn(
                     modifier = Modifier.fillMaxSize(),
                 ) {
+                    item {
+                        Box(
+                            modifier =
+                                Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 16.dp, vertical = 10.dp),
+                        ) {
+                            Icon(
+                                imageVector = Icons.Outlined.AccountCircle,
+                                contentDescription = null,
+                                tint = MaterialTheme.colorScheme.secondary,
+                                modifier =
+                                    Modifier
+                                        .size(40.dp)
+                                        .clip(RoundedCornerShape(10.dp))
+                                        .clickable { /* aksi */ }
+                                        .align(Alignment.CenterEnd),
+                            )
+                        }
+                    }
                     item {
                         Text(
                             modifier =
