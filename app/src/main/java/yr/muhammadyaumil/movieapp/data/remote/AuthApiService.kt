beb8a7ae1965eb68f9fabbf65d716a75b4784a08
yaumil <yaumilramkhadani@gmail.com>
2026-02-24 21:45:57 +0700

    make auth remote service

---
+++ app/src/main/java/yr/muhammadyaumil/movieapp/data/remote/AuthApiService.kt
@@ -0,0 +1,57 @@
+package yr.muhammadyaumil.movieapp.data.remote
+
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.auth.auth
+import io.github.jan.supabase.auth.providers.builtin.Email
+import io.github.jan.supabase.auth.user.UserInfo
+import javax.inject.Inject
+
+interface AuthApiServices {
+    suspend fun signInWithEmailAndPassword(
+        email: String,
+        password: String,
+    )
+
+    suspend fun signUpWithEmailAndPassword(
+        email: String,
+        password: String,
+    ): UserInfo?
+
+    suspend fun authWithGoogle()
+
+    suspend fun getCurrentUser(): UserInfo?
+
+    suspend fun logout()
+}
+
+class AuthApiServicesImpl
+    @Inject
+    constructor(
+        private val client: SupabaseClient,
+    ) : AuthApiServices {
+        override suspend fun signInWithEmailAndPassword(
+            email: String,
+            password: String,
+        ) = client.auth.signInWith(Email) {
+            this.email = email
+            this.password = password
+        }
+
+        override suspend fun signUpWithEmailAndPassword(
+            email: String,
+            password: String,
+        ) = client.auth.signUpWith(
+            Email,
+        ) {
+            this.email = email
+            this.password = password
+        }
+
+        override suspend fun authWithGoogle() {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun getCurrentUser(): UserInfo? = client.auth.currentUserOrNull()
+
+        override suspend fun logout() = client.auth.signOut()
+    }
