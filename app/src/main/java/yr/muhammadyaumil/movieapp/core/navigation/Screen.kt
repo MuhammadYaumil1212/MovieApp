405e0ffc29cce240278066948199e7a75cdbbcd9
yaumil <yaumilramkhadani@gmail.com>
2026-03-01 04:09:38 +0700

    create new route

--- app/src/main/java/yr/muhammadyaumil/movieapp/core/navigation/Screen.kt
+++ app/src/main/java/yr/muhammadyaumil/movieapp/core/navigation/Screen.kt
@@ -1,12 +1,18 @@
 package yr.muhammadyaumil.movieapp.core.navigation
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Bookmark
+import androidx.compose.material.icons.filled.Groups
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Theaters
+import androidx.compose.material.icons.outlined.Bookmark
+import androidx.compose.material.icons.outlined.Groups
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.outlined.Theaters
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class Screen(
@@ -25,7 +31,18 @@ sealed class Screen(
 
     object Profile : Screen("profile", "profile", Icons.Filled.Person, Icons.Outlined.Person)
 
+    object WatchParty :
+        Screen("watchParty", "watchParty", Icons.Filled.Theaters, Icons.Outlined.Theaters)
+
+    object Community : Screen("community", "community", Icons.Filled.Groups, Icons.Outlined.Groups)
+
+    object WatchList :
+        Screen("watchList", "Watch List", Icons.Filled.Bookmark, Icons.Outlined.Bookmark)
+
+    object FilmDetail :
+        Screen("filmDetail", "filmDetail", Icons.Filled.Person, Icons.Outlined.Person)
+
     companion object {
-        val bottomNavItems = listOf(Home, Settings)
+        val bottomNavItems = listOf(Home, WatchList, Community, Settings)
     }
 }
