e10085375715b83ee1ee65b6f33b98c0d080d75e
yaumil <yaumilramkhadani@gmail.com>
2026-03-01 21:58:53 +0700

    Delete unused compose

--- app/src/main/java/yr/muhammadyaumil/movieapp/ui/profile/screens/ProfileScreen.kt
+++ app/src/main/java/yr/muhammadyaumil/movieapp/ui/profile/screens/ProfileScreen.kt
@@ -1,52 +1,39 @@
 package yr.muhammadyaumil.movieapp.ui.profile.screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.KeyboardArrowRight
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.outlined.AccountCircle
-import androidx.compose.material.icons.outlined.PhotoCamera
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil3.compose.AsyncImage
 import yr.muhammadyaumil.movieapp.core.composables.AppScaffold
 import yr.muhammadyaumil.movieapp.core.composables.AppTextfield
 import yr.muhammadyaumil.movieapp.ui.profile.event.ProfileEvent
@@ -184,125 +171,6 @@ fun EditableProfileItem(
             value = displayValue,
             onClick = { onEditClick() },
         )
-    }
-}
-
-@Suppress("ktlint:standard:function-naming")
-@Composable
-fun ImageProfileEdit(state: ProfileState) {
-    Box(
-        modifier = Modifier.size(120.dp),
-    ) {
-        if (state.profileUrl.isNotEmpty()) {
-            AsyncImage(
-                modifier =
-                    Modifier
-                        .fillMaxSize()
-                        .clip(CircleShape)
-                        .background(Color(0xFF4A5144)),
-                model = state.profileUrl,
-                contentDescription = "Foto Profil",
-                contentScale = ContentScale.Crop,
-            )
-        } else {
-            Icon(
-                modifier =
-                    Modifier
-                        .fillMaxSize()
-                        .padding(5.dp)
-                        .clip(CircleShape)
-                        .background(Color(0xFF4A5144)),
-                tint = MaterialTheme.colorScheme.background,
-                imageVector = Icons.Outlined.AccountCircle,
-                contentDescription = "Profile Icon",
-            )
-        }
-
-        Surface(
-            shape = CircleShape,
-            color = Color.White,
-            modifier =
-                Modifier
-                    .size(36.dp)
-                    .clip(CircleShape)
-                    .align(Alignment.BottomEnd)
-                    .offset(
-                        x = 0.dp,
-                        y = 0.dp,
-                    ).clickable { /*camera edit button*/ },
-            shadowElevation = 2.dp,
-        ) {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier.fillMaxSize(),
-            ) {
-                Icon(
-                    imageVector = Icons.Outlined.PhotoCamera,
-                    contentDescription = "Change Photo Profile",
-                    tint = Color.Black,
-                    modifier = Modifier.size(20.dp),
-                )
-            }
-        }
-    }
-}
-
-@Suppress("ktlint:standard:function-naming")
-@Composable
-fun ProfileInfoCard(
-    modifier: Modifier = Modifier,
-    email: String?,
-    username: String?,
-    phone: String?,
-) {
-    Card(
-        modifier =
-            modifier
-                .fillMaxWidth()
-                .padding(start = 16.dp, end = 16.dp, top = 16.dp),
-        shape = RoundedCornerShape(12.dp),
-        colors = CardDefaults.cardColors(containerColor = Color.White),
-        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
-    ) {
-        Column(
-            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
-        ) {
-            InfoRowItem(
-                label = "Name",
-                value = username ?: "No Name",
-                onClick = { /* Handle click event */ },
-            )
-            HorizontalDivider(
-                modifier = Modifier.padding(vertical = 4.dp),
-                thickness = 1.dp,
-                color = Color.LightGray.copy(alpha = 0.3f),
-            )
-            InfoRowItem(
-                label = "Email",
-                value = email ?: "No Email",
-                onClick = { /* Handle click event */ },
-            )
-            HorizontalDivider(
-                modifier = Modifier.padding(vertical = 4.dp),
-                thickness = 1.dp,
-                color = Color.LightGray.copy(alpha = 0.3f),
-            )
-            InfoRowItem(
-                label = "Phone Number",
-                value = phone ?: "No Phone Number",
-                onClick = { /* Handle click event */ },
-            )
-            HorizontalDivider(
-                modifier = Modifier.padding(vertical = 4.dp),
-                thickness = 1.dp,
-                color = Color.LightGray.copy(alpha = 0.3f),
-            )
-            InfoRowItem(
-                label = "Change Password",
-                value = "*******",
-                onClick = { /* Handle click event */ },
-            )
-        }
     }
 }
 
