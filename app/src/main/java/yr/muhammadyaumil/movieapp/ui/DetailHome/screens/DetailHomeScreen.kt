773d03036afa89dcb115d6b492dc63437e835085
yaumil <yaumilramkhadani@gmail.com>
2026-03-16 00:36:09 +0700

    fix ui spacing

--- app/src/main/java/yr/muhammadyaumil/movieapp/ui/DetailHome/screens/DetailHomeScreen.kt
+++ app/src/main/java/yr/muhammadyaumil/movieapp/ui/DetailHome/screens/DetailHomeScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
@@ -89,12 +88,10 @@ fun DetailHomeScreen(
         onErrorConsumed = {
             onEvent(DetailHomeEvent.ResetError)
         },
-    ) { innerPadding ->
+    ) {
         Column(
             modifier =
                 Modifier
-                    .padding(innerPadding)
-                    .fillMaxSize()
                     .verticalScroll(scrollState),
         ) {
             Box(
