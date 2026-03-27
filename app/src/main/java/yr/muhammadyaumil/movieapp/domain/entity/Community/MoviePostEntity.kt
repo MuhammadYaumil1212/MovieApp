b00d402b8337490acef59dbdd4949052d2e2ffc9
yaumil <yaumilramkhadani@gmail.com>
2026-03-28 01:23:39 +0700

    fix model

--- app/src/main/java/yr/muhammadyaumil/movieapp/domain/entity/Community/MoviePostEntity.kt
+++ app/src/main/java/yr/muhammadyaumil/movieapp/domain/entity/Community/MoviePostEntity.kt
@@ -3,6 +3,5 @@ package yr.muhammadyaumil.movieapp.domain.entity.Community
 data class MoviePostEntity(
     val tmdbMovieId: Int,
     val userId: String,
-    val username: String,
     val content: String,
 )
